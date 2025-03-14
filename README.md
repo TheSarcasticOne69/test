b94036c63b1ade2f70d67ee40931fc3da939d844
TheSarcasticOne69 <shivamkushwaha197@gmail.com>
2025-03-14 13:17:23 +0530

    added a new file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # test
 this is a test directory
 -to me by me
+-by me to you and me
---
+++ test.py
@@ -0,0 +1,14 @@
+'''
+git status
+to check status of the file
+'''
+
+'''
+git add . or git add file_name
+to add file to the staging area
+'''
+
+'''
+git commit -m "say something"
+'''
+print("hello world")
